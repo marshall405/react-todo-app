fd8b81f8d389bc27ccfc427277b26a45fb9a00c8
Marshall Slemp <marshall.slemp@gmail.com>
2020-02-18 04:41:41 -0600

    Updates

---
+++ precache-manifest.ecaeb6b892436e71f594d166082c714d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e8ec85ff4ea9a893550d48033a345ea",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "88b3c1bb265c6322ab0b",
+    "url": "/react-todo-app/static/css/main.37715f2a.chunk.css"
+  },
+  {
+    "revision": "a995aa58ca054005b87d",
+    "url": "/react-todo-app/static/js/2.68fbb554.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-todo-app/static/js/2.68fbb554.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88b3c1bb265c6322ab0b",
+    "url": "/react-todo-app/static/js/main.b775cae0.chunk.js"
+  },
+  {
+    "revision": "d1eef49f46e602ccd621",
+    "url": "/react-todo-app/static/js/runtime-main.eaa02ba6.js"
+  }
+]);
